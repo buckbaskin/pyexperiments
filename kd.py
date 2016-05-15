376e103ca6cab74d83ef71cdd8bc5cf6b1826315
Buck Baskin <mobile.wbaskin@gmail.com>
2016-05-14 23:26:56 -0400

    Finalize basic interface (add_node, find_node)

--- kd.py
+++ kd.py
@@ -2,7 +2,7 @@
 Implement a general kdtree
 '''
 
-def generate_tree(data_class):
+def generate_tree_class(data_class):
     '''
     generates a tree 
     '''
@@ -24,14 +24,14 @@ def generate_tree(data_class):
         def __kd_set_right(self, right):
             self.__kd_right = right
 
-        def __kd_find_down(self, node):
+        def __kd_find_down(self, near_to):
             '''
             Iteratively searches the tree
             '''
             level_node = self
 
             while True:
-                if (new_node.at_depth(level_node.__kd_depth) < 
+                if (near_to.at_depth(level_node.__kd_depth) < 
                     level_node.at_depth(level_node.__kd_depth)):
                     # left
                     side = 'left'
@@ -48,7 +48,49 @@ def generate_tree(data_class):
                     else:
                         level_node = getattr(level_node, '__kd_'+side)
 
-        def __kd_add_node(self, new_node):
+        def __kd_find_up(self, near_to, best):
+            '''
+            Once I've found the bottom, go back up the kd-tree and try to find
+            a better node
+            '''
+            # best = current best node, leaf of the tree when this is called
+            level_node = best.__kd_parent
+            best_distance = near_to.distance(best)
+
+            while (level_node is not None) and (level_node.__kd_depth >= 0):
+                # check if the level node axis is closer than the best distance
+                depth = level_node.__kd_depth
+                min_potential_dist = near_to.at_depth(depth) - level_node.at_depth(depth)
+                if abs(min_potential_dist) < best_distance:
+                    # it could be closer
+                    if min_potential_dist < 0.0:
+                        side = 'left'
+                    else:
+                        side = 'right'
+
+                    # is the level_node a better option?
+                    if near_to.distance(level_node) < best_distance:
+                        # if the level_node is closer, update best, best_distance
+                        best = level_node
+                        best_distance = near_to.distance(level_node)
+
+                    # try to find a closer option
+                    other_side_best = getattr(self, '__kd_'+side).kd_find_node(near_to)
+                    if near_to.distance(other_side_best) < best_distance:
+                        # if the option is closer, update best, best_distance
+                        best = other_side_best
+                        best_distance = near_to.distance(other_side_best)
+                
+                # move up
+                level_node = level_node.__kd_parent
+
+            return best
+
+        def kd_find_node(self, near_to):
+            leaf_estimate = self.__kd_find_down(near_to)
+            return self.__kd_find_up(near_to, leaf_estimate)
+
+        def kd_add_node(self, new_node):
             '''
             Iteratively adds down the tree until it finds the place to put it
             '''
@@ -73,3 +115,5 @@ def generate_tree(data_class):
                         level_node = getattr(level_node, '__kd_'+side)
 
             return False
+
+    return KDTree
