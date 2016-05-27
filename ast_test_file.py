e8897c8d7617aa5c99e701e816f787677a1a9b94
Buck Baskin <mobile.wbaskin@gmail.com>
2016-05-27 11:47:37 -0400

    This is an example of breaking (should output 4)

--- ast_test_file.py
+++ ast_test_file.py
@@ -1,4 +1,7 @@
 def hello_world(a, b):
+    '''
+    This is still wrong, because the code 
+    '''
     if a:
         if a and b:
             return 1
@@ -6,6 +9,8 @@ def hello_world(a, b):
             return 0
         else:
             return -1
+    else:
+        return 0
     if b:
         return 2
     else:
